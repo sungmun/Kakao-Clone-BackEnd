a39c934d34cde40a6a4f0a56d2553ae18627abbc
강성문 <tjdans174@mealant.com>
2019-10-06 18:03:48 +0900

    Update AuthMiddleware.ts

--- src/util/AuthMiddleware.ts
+++ src/util/AuthMiddleware.ts
@@ -3,14 +3,21 @@ import { NotFoundTokenError } from './Error/NotFoundTokenError';
 import { User } from '../database/models/User.model';
 import { verify } from 'jsonwebtoken';
 import { secret } from '../../private-key.json';
+import { NotFoundUserError } from './Error/NotFoundUserError';
 
 export const auth = async (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers['x-access-token'] || req.query.token;
   if (!token) throw new NotFoundTokenError();
 
-  const profile = <User>verify(token, secret);
-  const userLoad = <User>await User.findByPk(profile.id);
-  req.body.profile = userLoad.get({ plain: true });
+  const id = <string>verify(token, secret);
 
+  const profile = await User.findByPk(id);
+
+  if (!profile) throw new NotFoundUserError();
+
+  req.body = {
+    ...req.body,
+    profile,
+  };
   next();
 };
